5de502139c145180f8863a7fd033d74f55b83f2e
yaozhong <272296321@qq.com>
2019-08-05 08:13:55 +0800

    更新log性能测试类

--- src/main/java/com/zyz/benchmark/LogBenchmark.java
+++ src/main/java/com/zyz/benchmark/LogBenchmark.java
@@ -1,7 +1,30 @@
 package com.zyz.benchmark;
 
+import com.zyz.logtest.LogTest;
+import org.openjdk.jmh.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by yaozheng on 19/8/4.
  */
+@BenchmarkMode(Mode.Throughput)
+@Warmup(iterations = 2)
+@Measurement(iterations = 1, time = 5, timeUnit = TimeUnit.SECONDS)
+@Threads(4)
+@Fork(1)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class LogBenchmark {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogTest.class);
+
+    @Benchmark
+    public void test() {
+        for (int i = 0; i < 10; i++) {
+            LOGGER.info("hello log4j");
+        }
+
+    }
 }
--- src/main/java/com/zyz/logtest/LogTest.java
+++ src/main/java/com/zyz/logtest/LogTest.java
@@ -2,13 +2,23 @@ package com.zyz.logtest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class LogTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LogTest.class);
 
     public static void main(String[] args) {
+        String trace_id = "trace_id";
+        MDC.put(trace_id,"001");
+
         LOGGER.info("hello");
+        MDC.remove(trace_id);
+
+        MDC.put(trace_id,"002");
+
         LOGGER.info("world");
+        MDC.remove(trace_id);
+
     }
 }
